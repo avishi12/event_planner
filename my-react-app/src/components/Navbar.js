a502bb72d4b0086728525775d03e5ed089ed8a86
UGSNBandara <sulithanb119@gmail.com>
2026-02-17 17:48:09 +0530

    changed

--- my-react-app/src/components/Navbar.js
+++ my-react-app/src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <Link to="/">Deshabandu</Link>
+        <Link to="/">Deshabandu Thennnn</Link>
       </div>
       <div className="navbar-links">
         <Link to="/Home">Home</Link>
